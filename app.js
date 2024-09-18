40d008571c1c85578c043f960ef4e57ddc1c3dcf
estefani <estefanixuxu25@gmail.com>
2024-09-18 10:24:09 -0300

    Configuração do banco de dados utilizando  o Prisma

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
 const express = require('express')
 const app = express()
 
-const database = require('./config/db')
-
-database
-    .authenticate()
-    .then(() =>{
-        console.log("Banco de dados conectado com sucesso")
-    }).catch((msgErro) =>{
-        console.log(msgErro)
-    })
-
 module.exports = app
